b5c4a5a52a91b26387d4547ea745808866276bb7
rodrigo0o0 <sandeski47@gmail.com>
2023-04-28 03:59:26 -0300

    projeto finalizado

--- js/script.js
+++ js/script.js
@@ -1,7 +1,34 @@
+function play(seletorAudio){
+    const elemento = document.getElementById(seletorAudio);
+    if(elemento && elemento.localName == 'audio'){
 
+        elemento.play();
+    }else{
+        console.log("elemento não encontrado")
+    }
 
-
-function play(id){
-    var obj = document.getElementById(id);
-    obj.play();
 }
+
+
+
+
+const listaTeclas = document.querySelectorAll('.tecla');
+
+for (let i = 0; i < listaTeclas.length; i++) {
+    const classe = listaTeclas[i].classList[1]
+    const tecla = listaTeclas[i];
+    listaTeclas[i].onclick = function (){
+        play('som_' + classe);
+
+    }
+    tecla.onkeydown = function (event) {
+        if(event.code == 'Space' || event.code == 'Enter'){
+            tecla.classList.add('ativa');
+        }
+    }
+    tecla.onkeyup = function (){
+        tecla.classList.remove('ativa')
+    }
+    
+}
+
